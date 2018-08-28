f0b693542c8693d20e287c4c14c6efb0a278680a
Janusz Chudzynski <janusz@izotx.com>
2018-08-28 16:56:50 -0500

    Started Adding Delegate Code

--- EOSBlockExplorer/EOSBlockExplorer/BlockchainClasses.swift
+++ EOSBlockExplorer/EOSBlockExplorer/BlockchainClasses.swift
@@ -8,7 +8,44 @@
 
 import Foundation
 
-struct BlockInfo:Decodable{
+protocol Producer{
+    var PRODUCER_URL:String {get}
+    var INFO_ENDPOINT:String {get}
+    var GET_BLOCK_ENDPOINT:String {get}
+}
+
+class NewYorkProducer:Producer{
+    var PRODUCER_URL = "https://api.eosnewyork.io"
+    var INFO_ENDPOINT = "https://api.eosnewyork.io/v1/chain/get_info"
+    var GET_BLOCK_ENDPOINT = "https://api.eosnewyork.io/v1/chain/get_block"
+}
+
+
+
+enum EOSError: Error {
+    case unknownError
+    case connectionError
+    case invalidCredentials
+    case invalidRequest
+    case notFound
+    case invalidResponse
+    case serverError
+    case serverUnavailable
+    case timeOut
+    case parsingJSON
+    case currentlyDownloading
+}
+
+
+
+struct ChainInfo:Decodable{
+    let lastBlock:Int
+    enum CodingKeys : String, CodingKey {
+        case lastBlock = "last_irreversible_block_num"
+    }
+}
+
+struct Block:Decodable{
     let block_num:Int
     let previous:String
     let producer:String
@@ -20,113 +57,6 @@ struct Transaction:Decodable{
     let status:String
 }
 
-struct API{
-    static let PRODUCER_URL = "https://api.eosnewyork.io"
-    static let INFO_ENDPOINT = "v1/chain/get_info"
-    static let GET_BLOCK_ENDPOINT = "v1/chain/get_block"
-}
-
-
-struct ChainInfo:Decodable{
-    let lastBlock:Int
-    enum CodingKeys : String, CodingKey {
-        case lastBlock = "last_irreversible_block_num"
-    }
-}
-
-class APIOperations
-{
-    
-    var client:HttpClient!
-    var counter = 0
-    var max = 20
-    var array = [BlockInfo]()
-    var pending = [String:Bool]()
-    
-    
-    init(_ client:HttpClient){
-        self.client = client
-    }
-    
-    func getGeneralInfo(completion:@escaping (ChainInfo?)->Void){
-        let urlString = "\(API.PRODUCER_URL)/\(API.INFO_ENDPOINT)"
-        
-        guard let url = URL(string:urlString ) else{
-            print("error \(urlString)")
-            
-            return
-        }
-        client.post(url: url, body: nil) { (data, error) in
-            
-            guard let data = data, error == nil else {
-                print("error=\(error.debugDescription)")
-                return
-            }
-            
-            guard let info = try? JSONDecoder().decode(ChainInfo.self, from: data) else{
-                print("debug description")
-                return
-            }
-            //        print(info)
-            completion(info)
-        }
-    }
-    
-    func getBlockContents(block:String,  completion: @escaping (BlockInfo?)->Void){
-        let urlString = "\(API.PRODUCER_URL)/\(API.GET_BLOCK_ENDPOINT)"
-        
-        guard let url = URL(string:urlString ) else{
-            print("error \(urlString)")
-            
-            return
-        }
-        
-        let json: [String: Any] = ["block_num_or_id":block]
-        let jsonData = try? JSONSerialization.data(withJSONObject: json)
-        
-        client.post(url: url, body: jsonData) { (data, error) in
-            guard let data = data, error == nil else {
-                print("error=\(error.debugDescription)")
-                return
-            }
-            
-            let info = try! JSONDecoder().decode(BlockInfo.self, from: data)
-            print("Block")
-            print(info.block_num)
-            completion(info)
-        }
-    }
-    
-    
-    func downloadBlockData(blockid:String){
-        print("Downloading")
-        print(array.count)
-        print("\n \n")
-        
-        if array.count == max{
-            
-            return
-        }
-        pending[blockid] = true
-        
-        getBlockContents(block:blockid) { (info) in
-            if let info = info{
-                //remove from pending
-                //add to array
-                //Call download 20
-                print("Downloaded \(info.block_num)")
-                self.array.append(info)
-                self.downloadBlockData(blockid: info.previous)
-            }else{
-                //Don't call anything
-                print("Some Error")
-            }
-        }
-    }
-
-    
-    
-}
 
 
 /**Based on Network Unit Testing in Swift*/
@@ -143,13 +73,148 @@ class HttpClient {
         request.httpMethod = "POST"
         request.httpBody = body
         let task = session.dataTask(with: request) { (data, response, error) in
-            
-            if let data = data, let body_response = String(data: data, encoding: String.Encoding.utf8) {
-                print(body_response)
-            }
-            
             callback(data, error)
         }
         task.resume()
+    }
+}
+
+
+/**Responsible for blockchain operations */
+class BlockchainOperations{
+    private let client:HttpClient
+    private var max:Int!
+    private var blocks = [Block]()
+    private var isRunning:Bool = false
+    private var producer:Producer!
+    
+    //Completion blocks
+    typealias blocksCompleteClosure = ( _ blocks: [Block], _ error: Error?)->Void
+    typealias blockCompleteClosure = ( _ blocks: Block?, _ error: Error?)->Void
+    typealias chainCompleteClosure = ( _ chain: ChainInfo?, _ error: Error?)->Void
+    
+    //Default
+    init(){
+        let session = URLSession.shared
+        self.client = HttpClient(session: session)
+        self.producer = NewYorkProducer()
+    }
+    
+    init(session:URLSession,producer:Producer) {
+        self.client = HttpClient(session: session)
+        self.producer = producer
+    }
+    
+    
+    
+    func downloadBlocks(_ blocks_num:Int, completion: @escaping blocksCompleteClosure ){
+        if isRunning{
+            completion(self.blocks, EOSError.currentlyDownloading)
+            return
+        }
+        
+        //Getting n blocks
+        max = blocks_num
+        
+        isRunning = true
+        
+        //removing old blocks
+        self.blocks.removeAll()
+        
+        getGeneralInfo { (info,error) in
+            
+            if let error = error
+            {
+                print(error)
+                self.isRunning = false
+                completion(self.blocks,error)
+                return
+            }
+            if let info = info {
+                let head_id = String(info.lastBlock)
+                //Finally start downloading
+                self.download(blockid: head_id, { (_blocks, _error) in
+                    self.isRunning = false
+                    completion(_blocks,_error)
+                    
+                })
+            }else{
+                self.isRunning = false
+                completion(self.blocks, EOSError.unknownError)
+            }
+        }
+    }
+    
+    /**Responsible for downloading block with given block id*/
+    func download(blockid:String, _ completion:@escaping blocksCompleteClosure){
+        if blocks.count == max{
+            completion(self.blocks,nil)
+            return
+        }
+        
+        getBlockContents(block:blockid) { (info,error) in
+            if let info = info{
+                self.blocks.append(info)
+                self.download(blockid: info.previous, completion)
+            }else{
+                completion(self.blocks,error)
+            }
+        }
+    }
+    
+    
+    /**Responsible for downloading information about latest information about chain/ block*/
+    func getGeneralInfo(completion:@escaping chainCompleteClosure){
+        let urlString = self.producer.INFO_ENDPOINT
+        
+        guard let url = URL(string:urlString ) else{
+            print("error \(urlString)")
+            
+            return
+        }
+        client.post(url: url, body: nil) { (data, error) in
+            
+            guard let data = data, error == nil else {
+                print("error=\(error.debugDescription)")
+                completion(nil, EOSError.invalidResponse)
+                return
+            }
+            
+            guard let info = try? JSONDecoder().decode(ChainInfo.self, from: data) else{
+                completion(nil, EOSError.parsingJSON)
+                return
+            }
+            
+            completion(info,nil)
+        }
+    }
+    
+    func getBlockContents(block:String,  completion: @escaping blockCompleteClosure){
+        let urlString = self.producer.GET_BLOCK_ENDPOINT
+        
+        guard let url = URL(string:urlString ) else{
+            print("error \(urlString)")
+            completion(nil,EOSError.invalidRequest)
+            return
+        }
+        
+        //Pass Block Number/id in JSON
+        let json: [String: Any] = ["block_num_or_id":block]
+        let jsonData = try? JSONSerialization.data(withJSONObject: json)
+        
+        client.post(url: url, body: jsonData) { (data, error) in
+            guard let data = data, error == nil else {
+                print("error=\(error.debugDescription)")
+                completion(nil,error)
+                return
+            }
+            
+            guard  let info = try? JSONDecoder().decode(Block.self, from: data) else{
+                completion(nil,EOSError.parsingJSON)
+                return
+            }
+            
+            completion(info,nil)
+        }
     }
 }
--- EOSBlockExplorer/EOSBlockExplorer/ViewControllers/BlockExplorerViewController.swift
+++ EOSBlockExplorer/EOSBlockExplorer/ViewControllers/BlockExplorerViewController.swift
@@ -8,57 +8,115 @@
 
 import UIKit
 
-class BlockExplorerViewController: UIViewController {
+enum CellIds:String{
+   case BlockCell = "BlockCell"
+}
+
+
+class BlockExplorerViewController: UIViewController, UITableViewDelegate {
     @IBOutlet weak var tableView: UITableView!
+    private var blockchainops:BlockchainOperations!
+    private var datasource = BlockDataSource()
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
         // Do any additional setup after loading the view.
+        //setup table view
+        self.blockchainops = BlockchainOperations()
+
+
+        //Setup TableView and Register the cell
+        tableView.register(UITableViewCell.self, forCellReuseIdentifier: CellIds.BlockCell.rawValue)
+        tableView.dataSource = datasource
+        tableView.delegate = self
+
+        //Setup UI
+        let refreshItem = UIBarButtonItem(barButtonSystemItem: .refresh, target: self, action: #selector(refreshBlocksAction(_:)) )
+        navigationItem.rightBarButtonItem = refreshItem
+        self.title =  "EOS Block Explorer"
+    
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+
+    
+    func presentAlert(text:String){
+        let alert = UIAlertController(title: "Message", message:text, preferredStyle: .alert)
+        alert.addAction(UIAlertAction(title: "OK", style: .default, handler: nil))
+        self.present(alert, animated: true)
+    }
+    
+    @objc @IBAction func refreshBlocksAction(_ sender: Any) {
+
+        UIApplication.shared.isNetworkActivityIndicatorVisible = true
+        //Avoiding Retain Cycle using weak self
+        self.blockchainops.downloadBlocks(20) { [weak self] (blocks, error) in
+
+            //A lot of networking calls, switch it to the Main Thread
+            DispatchQueue.main.async {
+                UIApplication.shared.isNetworkActivityIndicatorVisible = false
+                if let error = error{
+                    //present error to the user
+                    self?.presentAlert(text: error.localizedDescription)
+                }
+                
+                self?.datasource.blocks = blocks
+                self?.tableView.reloadData()
+            }
+        }
+    }
     
 
-    /*
     // MARK: - Navigation
 
     // In a storyboard-based application, you will often want to do a little preparation before navigation
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         // Get the new view controller using segue.destinationViewController.
         // Pass the selected object to the new view controller.
+        
     }
-    */
 
 }
 
-class Blockchain{
-    var blocks:Int?
+extension BlockExplorerViewController{
     
 }
 
 
-class BlockDataSource:UITableViewDataSource{
-    var blockchain = Blockchain()
+
+class BlockDataSource: NSObject, UITableViewDataSource{
+    
+    var blocks = [Block]()
     
     func numberOfSectionsInTableView(tableView: UITableView) -> Int
     {
         return 1
     }
     
-    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int
     {
-        return dataStore.allFlavors().count
+        return blocks.count
     }
     
-    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell
     {
-        let flavor = dataStore.allFlavors()[indexPath.row]
-        let cell = tableView.dequeueReusableCellWithIdentifier("IceCreamListCell", forIndexPath: indexPath)
-        cell.textLabel?.text = flavor
+        let block = blocks[indexPath.row]
+        let cell = tableView.dequeueReusableCell(withIdentifier: CellIds.BlockCell.rawValue, for: indexPath as IndexPath)
+        if indexPath.row == 0{
+            cell.backgroundColor = UIColor.gray
+            cell.textLabel?.textColor = UIColor.white
+        }
+        else{
+            cell.backgroundColor = UIColor.white
+            cell.textLabel?.textColor = UIColor.darkGray
+
+        }
+        cell.textLabel?.text = String(block.block_num)
+        cell.accessoryType = .detailDisclosureButton
+
         return cell
     }
     
